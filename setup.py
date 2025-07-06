6f39b2e0514b76ea0cdfdb53f3f2f0c34f7b6303
catngh <hoangcat622001@gmail.com>
2025-07-06 12:34:52 +0700

    fix: cicd not running

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ocelot_log_service",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        # List dependencies from requirements.txt here
+        "fastapi",
+        "uvicorn",
+        "boto3",
+        "pymongo",
+        "python-jose",
+        "passlib",
+        "python-dotenv",
+        "pydantic-settings",
+        "opensearch-py",
+        "requests-aws4auth",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+        ]
+    },
+) 
