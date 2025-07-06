5580e2a44e4888557c6d129625d6652655e5a0e7
catngh <hoangcat622001@gmail.com>
2025-07-07 00:53:10 +0700

    refactor: remove unused code

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import logs, tenants, stats
+from app.api.v1.endpoints import logs, tenants
 
 api_router = APIRouter()
 
 # Include routers for different resources
 api_router.include_router(logs.router, prefix="/logs", tags=["logs"])
 api_router.include_router(tenants.router, prefix="/tenants", tags=["tenants"])
-api_router.include_router(stats.router, prefix="/stats", tags=["stats"])
